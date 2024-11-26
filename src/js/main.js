5a7c256a5b29cfd87e44638d32ddee2e9a86c9f6
Abderrahmane Rabeh <rabehlife144@gmail.com>
2024-11-26 13:03:27 +0100

    #5 Faire un animation siple et fluid that que ouvrire ou fermer sideBar

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,22 @@
 let close_sidebar = document.getElementById("close_sidebar");
 
+let toggle = true;
 close_sidebar.addEventListener("click", () => {
-    right.style.width = "10%";
-    right.classList.add("grid-cols-1");
-    right.style.overflow = "scroll";
-    left.style.width = "90%";
-
+    if (toggle) {
+        right.style.width = "10%";
+        right.classList.add("grid-cols-1");
+        right.style.overflow = "scroll";
+        left.style.width = "90%";
+        right.style.transition = "width 0.5s ease-in-out";
+        toggle = false;
+    } else {
+        right.style.width = "20%";
+        right.classList.remove("grid-cols-1");
+        setTimeout(() => {
+            left.style.width = "80%";
+        }, 1000)
+        right.style.transition = "width 0.5s ease-in-out";
+        toggle = true;
+    }
 })
+
