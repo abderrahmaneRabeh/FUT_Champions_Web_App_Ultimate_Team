4694f3d41d0c3b0a5f4377223b4adadcfc621cb9
Abderrahmane Rabeh <rabehlife144@gmail.com>
2024-11-26 15:32:51 +0100

    #5 resolver un bug sur les card dans sideBar

--- src/js/main.js
+++ src/js/main.js
@@ -3,18 +3,22 @@ let close_sidebar = document.getElementById("close_sidebar");
 let toggle = true;
 close_sidebar.addEventListener("click", () => {
     if (toggle) {
-        right.style.width = "10%";
+        right.style.transition = "width 0.5s ease-in-out";
+        right.classList.remove("grid-cols-2");
         right.classList.add("grid-cols-1");
+        close_sidebar.style.width = "2rem";
+        close_sidebar.style.paddingBottom = "1rem";
+        right.style.width = "10%";
         right.style.overflow = "scroll";
         left.style.width = "90%";
-        right.style.transition = "width 0.5s ease-in-out";
         toggle = false;
     } else {
         right.style.width = "20%";
         right.classList.remove("grid-cols-1");
+        right.classList.add("grid-cols-2");
         setTimeout(() => {
             left.style.width = "80%";
-        }, 1000)
+        }, 300)
         right.style.transition = "width 0.5s ease-in-out";
         toggle = true;
     }
