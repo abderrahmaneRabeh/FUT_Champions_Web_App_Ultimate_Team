5804f845a2114db7536e053412da8fb288b9f2b8
Abderrahmane Rabeh <rabehlife144@gmail.com>
2024-11-28 12:10:37 +0100

    #8 Implémenter un Pop-Up qui afficher un list filtrer par position avant pour facilite UX

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,33 @@
 let close_sidebar = document.getElementById("close_sidebar");
 
+// Les position des joueurs 
+
+let GK = document.getElementById("GK");
+let LB = document.getElementById("LB");
+let RB = document.getElementById("RB");
+let DC_1 = document.getElementById("DC1");
+let DC_2 = document.getElementById("DC2");
+let MC = document.getElementById("CM");
+let CAM = document.getElementById("CAM");
+let CDM = document.getElementById("CMD");
+let RW = document.getElementById("RW");
+let LW = document.getElementById("LW");
+let ST = document.getElementById("ST");
+
+// substituer les joueurs
+
+let sub_1 = document.getElementById("substitut-1");
+let sub_2 = document.getElementById("substitut-2");
+let sub_3 = document.getElementById("substitut-3");
+let sub_4 = document.getElementById("substitut-4");
+let sub_5 = document.getElementById("substitut-5");
+let sub_6 = document.getElementById("substitut-6");
+let sub_7 = document.getElementById("substitut-7");
+
+// tout les remplacent des joueurs
+
+let sub = [sub_1, sub_2, sub_3, sub_4, sub_5, sub_6, sub_7];
+
 let toggle = true;
 close_sidebar.addEventListener("click", () => {
     if (toggle) {
@@ -27,8 +55,270 @@ close_sidebar.addEventListener("click", () => {
 
 //Récuperer les joueurs dans localStorage
 
-if (localStorage.getItem("players")) {
-    let players = JSON.parse(localStorage.getItem("players"));
-    console.log(players);
+let players = [];
 
+if (localStorage.getItem("players")) {
+    players = JSON.parse(localStorage.getItem("players"));
+    console.log(players);
+}
+
+// Ajouter le joueur à un position dans l'equipe
+
+function filterPlayersByPosition(position) {
+    return players.filter(player => player.position === position);
+}
+
+GK.addEventListener("click", () => {
+
+    setUpPopUp();
+
+    let players = filterPlayersByPosition("GK");
+    displayPlayers(players);
+
+    setTimeout(() => {
+        players_pop_up.classList.remove("flex");
+        players_pop_up.classList.add("hidden");
+    }, 1500)
+
+
+});
+LB.addEventListener("click", () => {
+
+    setUpPopUp();
+
+    let players = filterPlayersByPosition("LB");
+
+    displayPlayers(players);
+
+    setTimeout(() => {
+        players_pop_up.classList.remove("flex");
+        players_pop_up.classList.add("hidden");
+    }, 1500)
+
+
+});
+RB.addEventListener("click", () => {
+
+    setUpPopUp();
+
+    players_pop_up.innerHTML = "";
+
+    let players = filterPlayersByPosition("RB");
+
+    displayPlayers(players);
+
+    setTimeout(() => {
+        players_pop_up.classList.remove("flex");
+        players_pop_up.classList.add("hidden");
+    }, 1500)
+
+});
+DC_1.addEventListener("click", () => {
+
+    setUpPopUp();
+
+    let players = filterPlayersByPosition("DC");
+
+    displayPlayers(players);
+
+    setTimeout(() => {
+        players_pop_up.classList.remove("flex");
+        players_pop_up.classList.add("hidden");
+    }, 1500)
+
+});
+DC_2.addEventListener("click", () => {
+
+    setUpPopUp();
+
+    let players = filterPlayersByPosition("DC");
+
+    displayPlayers(players);
+
+    setTimeout(() => {
+        players_pop_up.classList.remove("flex");
+        players_pop_up.classList.add("hidden");
+    }, 1500)
+
+
+});
+MC.addEventListener("click", () => {
+
+    setUpPopUp();
+
+    let players = filterPlayersByPosition("CM");
+
+    displayPlayers(players);
+
+    setTimeout(() => {
+        players_pop_up.classList.remove("flex");
+        players_pop_up.classList.add("hidden");
+    }, 1500)
+
+});
+CAM.addEventListener("click", () => {
+
+    setUpPopUp();
+
+    let players = filterPlayersByPosition("CAM");
+
+    displayPlayers(players);
+
+    setTimeout(() => {
+        players_pop_up.classList.remove("flex");
+        players_pop_up.classList.add("hidden");
+    }, 1500);
+
+});
+CDM.addEventListener("click", () => {
+
+    setUpPopUp();
+
+    let players = filterPlayersByPosition("CDM");
+
+    displayPlayers(players);
+
+    setTimeout(() => {
+        players_pop_up.classList.remove("flex");
+        players_pop_up.classList.add("hidden");
+    }, 1500)
+
+});
+RW.addEventListener("click", () => {
+
+    setUpPopUp();
+
+    let players = filterPlayersByPosition("RW");
+
+    displayPlayers(players);
+
+    setTimeout(() => {
+        players_pop_up.classList.remove("flex");
+        players_pop_up.classList.add("hidden");
+    }, 1500)
+
+});
+LW.addEventListener("click", () => {
+    setUpPopUp();
+
+    let players = filterPlayersByPosition("LW");
+
+    displayPlayers(players);
+
+    setTimeout(() => {
+        players_pop_up.classList.remove("flex");
+        players_pop_up.classList.add("hidden");
+    }, 1500)
+
+});
+ST.addEventListener("click", () => {
+
+    setUpPopUp();
+
+    let players = filterPlayersByPosition("ST");
+
+    displayPlayers(players);
+
+    setTimeout(() => {
+        players_pop_up.classList.remove("flex");
+        players_pop_up.classList.add("hidden");
+    }, 1500)
+
+});
+
+// Ajouter le remplacant à un position dans l'equipe
+
+function displayPlayers(players = []) {
+
+    players.forEach(player => {
+        players_pop_up.innerHTML += `
+        <div class="relative cursor-pointer hover:scale-95 transition-transform flex flex-col items-center justify-center text-white card_item" id="${player.id}">
+            <div class="flex">
+                <div class="flex flex-col items-center absolute left-[10px] top-[20px] rate_item">
+                    <span class="font-bold">${player.Rate}</span>
+                    <span>${player.position}</span>
+                </div>
+                <img src="${player.image}"
+                    alt="arsenal player" class="w-12 h-12 mt-2">
+            </div>
+            <p class="font-bold name_joueur">${player.name}</p>
+
+          ${player.position == "GK" ? `
+           <div class="flex gap-1 text-xs stats">
+                <div class="text-center pace">
+                    <p>REF</p>
+                    <p class="font-bold">${player.status.ref}</p>
+                </div>
+                <div class="text-center shot">
+                    <p>POS</p>
+                    <p class="font-bold">${player.status.pos}</p>
+                </div>
+                <div class="text-center PAS">
+                    <p>KICK</p>
+                    <p class="font-bold">${player.status.kick}</p>
+                </div>
+                <div class="text-center DRI">
+                    <p>HAND</p>
+                    <p class="font-bold">${player.status.hand}</p>
+                </div>
+                <div class="text-center PHY">
+                    <p>PEN</p>
+                    <p class="font-bold">${player.status.pen}</p>
+                </div>
+            </div>
+          `: `
+          
+          <div class="flex gap-2 text-xs stats">
+    <div class="text-center pace">
+      <p>PAC</p>
+      <p class="font-bold">${player.status.pace}</p>
+    </div>
+    <div class="text-center shot">
+      <p>SHO</p>
+      <p class="font-bold">${player.status.shot}</p>
+    </div>
+    <div class="text-center PAS">
+      <p>PAS</p>
+      <p class="font-bold">${player.status.pas}</p>
+    </div>
+    <div class="text-center DRI">
+      <p>DEF</p>
+      <p class="font-bold">${player.status.dri}</p>
+    </div>
+    <div class="text-center PHY">
+      <p>PHY</p>
+      <p class="font-bold">${player.status.phy}</p>
+    </div>
+  </div>`}
+
+            <div class="flex gap-1 mb-2">
+                <img class="w-3 h-2"
+                    src="${player.nationalite}"
+                    alt="nationalité flag">
+
+                <img src="https://cdn3.futbin.com/content/fifa25/img/league/dark/13.png?fm=png&amp;ixlib=java-2.1.0&amp;verzion=1&amp;w=44&amp;s=18fa65dd539f7a4c11622711afeecb1f"
+                    alt="" class="w-3 h-2">
+                <img class="w-3 h-2"
+                    src="https://cdn3.futbin.com/content/fifa25/img/clubs/dark/1.png?fm=png&amp;ixlib=java-2.1.0&amp;verzion=2&amp;w=44&amp;s=7b4e8535a9ec8a9fc1a5d2ef83e7322a"
+                    alt="club logo">
+            </div>
+        </div>
+        
+        `
+    });
+
+}
+
+//set up pop up 
+
+function setUpPopUp() {
+
+    players_pop_up.innerHTML = "";
+
+    players_pop_up.classList.remove("hidden");
+    players_pop_up.classList.add("flex");
+
+    if (players.length == 0) {
+        players_pop_up.innerHTML = `<div class="w-full mt-32 font-bold text-xl text-center">Aucun joueur, ajouté nouveau joueur</div>`;
+    }
 }
